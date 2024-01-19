af4a6bb717795796e08ee8e91b8fff8b6c07177d
developerleonardo <leo.11hnz@gmail.com>
2024-01-19 12:24:06 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  theme: {
+    fontFamily: {
+      Montserrat: ['Montserrat', "sans-serif"]
+    },
+    colors: {
+      ...colors,
+      "primary": "#CC2D4A",
+      "secondary": "#8FA206",
+      "tertiary": "#61AEC9",
+    },
+    extend: {
+      backgroundImage: {
+        "sanFrancisco": "url('../img/sanFrancisco.jpg')",
+        "sanFranciscoDesktop": "url('../img/sanFranciscoDesktop.jpg')",
+        "bali": "url('../img/bali.jpg')",
+        "chicago": "url('../img/chicago.jpg')",
+        "europe": "url('../img/europe.jpg')",
+        "iceland": "url('../img/iceland.jpg')",
+        "LA": "url('../img/LA.jpg')",
+        "miami": "url('../img/miami.jpg')",
+        "new_york": "url('../img/new_york.jpg')",
+        "norway": "url('../img/norway.jpg')",
+        "seattle": "url('../img/seattle.jpg')",
+        "switzerland": "url('../img/switzerland.jpg')",
+        "sydney": "url('../img/sydney.jpg')",
+        "yosemite": "url('../img/yosemite.jpg')",
+
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
+
